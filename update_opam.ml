5392e0a55d103b4700d20bc7f0674dd6da9db0ff
Marcello Seri <marcello.seri@gmail.com>
2024-02-09 10:11:06 +0100

    Fmt
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>

--- update_opam.ml
+++ update_opam.ml
@@ -21,19 +21,6 @@ let [@tail_mod_cons] rec input_trimmed_lines ic =
 let read filename =
   In_channel.with_open_text filename input_trimmed_lines
 
-let _save_opam_file opam_package content =
-  let name = OpamFile.OPAM.name opam_package |> OpamPackage.Name.to_string in
-  let version = OpamFile.OPAM.version opam_package |> OpamPackage.Version.to_string in
-  let url = Option.map (OpamUrl.to_string) (OpamFile.OPAM.get_url opam_package) in
-  let extension = match Option.map Filename.extension url with
-    | Some ".gz" -> ".tar.gz"
-    | Some ext -> ext 
-    | None -> failwith "Unable to infer file extension from url"
-  in
-  let filename = name ^ "-" ^ version ^ extension in
-  write content filename;
-  name ^ "." ^ version, filename
-
 let () = 
   let opam_repo = get_opam_repo () in
   (* "filelist.txt" is a list name.version *)
@@ -53,25 +40,4 @@ let () =
     let url_p = OpamFile.URL.create ~checksum (OpamUrl.of_string url) in
     let opam_p = OpamFile.OPAM.with_url url_p opam_p in
     OpamFile.OPAM.write fn opam_p) ps
-(*
-  let ns = Lwt_list.filter_map_p (fun p ->
-    let open Lwt.Syntax in
-    let opam_p = get_opam_file opam_repo p in
-    if OpamFilter.to_string (OpamFile.OPAM.available opam_p) = "false" then Lwt.return_none
-    else begin
-      let url = process opam_p in
-      let* content = retry url caches in
-      match content with
-      | Some f -> begin
-        try
-          Lwt.return_some (save_package_file opam_p f)
-        with e -> 
-          print_endline (Printexc.to_string e);
-          Lwt.return_none
-        end
-      | None -> print_endline ("Could not save " ^ (OpamPackage.to_string p)); Lwt.return_none
-      end) ps
-  in
-  let ns = Lwt_main.run ns |> List.map (fun (p ,n) -> p ^ " https://github.com/ocaml/opam-source-archives/raw/main/" ^ n) in
-  write (String.concat "\n" ns) "saved_files.txt"
-*)
+
